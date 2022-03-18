cb1e44bbb6aa220d480c1c04f98288f2636709ca
tommydotcf <100290912+tommydotcf@users.noreply.github.com>
2022-03-18 17:03:14 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,9 +4,6 @@
 variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
-variable "private_key_path" {}
-variable "compartment_ocid" {}
-variable "region" {}
 
 variable "app_name" {
   default     = "oci-function-cicd"
