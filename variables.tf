f38d16e424c1a580efc600bd24f1edad6e0bd8c5
tommydotcf <100290912+tommydotcf@users.noreply.github.com>
2022-03-18 17:16:07 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,8 +2,14 @@
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
 variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
+
+## variable "user_ocid" {}
+## variable "fingerprint" {}
+##variable "private_key_path" {}
+
+variable "compartment_ocid" {}
+variable "region" {}
+
 
 variable "app_name" {
   default     = "oci-function-cicd"
